d7e46d52c340e18a4a2a84a4b42891e14e7f4be7
ZachBrader <ZachBrader@hotmail.com>
2020-03-06 18:11:09 -0800

    Adding previously ignored files

---
+++ app/blueprints/home/routes.py
@@ -0,0 +1,17 @@
+from flask_login import login_required
+from flask import render_template
+
+from app.blueprints.home import home
+from app.models import User
+
+@home.route('/')
+@home.route('/index')
+def index():
+    return render_template("home/index.html")
+
+
+@home.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
