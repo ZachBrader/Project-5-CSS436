1913bb7c71c88adf651e502916ecb0297c535cae
ZachBrader <ZachBrader@hotmail.com>
2020-03-06 20:01:41 -0800

    Updating visuals

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -7,11 +7,37 @@ from app.models import User
 @home.route('/')
 @home.route('/index')
 def index():
-    return render_template("home/index.html")
+    pokeList = [{
+            'username': 'Zach',
+            'pokemon': ['Pichu', 'Pichu', 'Pichu']
+        },
+        {
+            'username': 'Andrei',
+            'pokemon': ['Magikarp']
+        },
+        {
+            'username': 'Thom',
+            'pokemon': ['Charizard']
+        }
+    ]
+    return render_template("home/index.html", pokeList=pokeList)
 
 
 @home.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('home/user.html', user=user)
+    pokeList = [{
+        'username': 'Zach',
+        'pokemon': ['Pichu', 'Pichu', 'Pichu']
+    },
+        {
+            'username': 'Andrei',
+            'pokemon': ['Magikarp']
+        },
+        {
+            'username': 'Thom',
+            'pokemon': ['Charizard']
+        }
+    ]
+    return render_template('home/user.html', user=user, pokeList=pokeList)
