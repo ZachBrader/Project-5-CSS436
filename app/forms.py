9a47a0cbb39361d6fac09323d7dc1ad06b524d23
thongle7 <thongle7@uw.edu>
2020-03-14 18:00:27 -0700

    Add the list of pokemon to select

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
-
+from app import pokemonList
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -31,18 +31,25 @@ class RegistrationForm(FlaskForm):
 
 
 class PokemonTeamBuilder(FlaskForm):
+    mychoices = pokemonList.pokemon_list
     teamname = StringField('Team Name', validators=[DataRequired()])
-    pokemon1 = StringField('Pokemon #1 Name', default="Magikarp", validators=[])
+    #pokemon1 = StringField('Pokemon #1 Name', default="Magikarp", validators=[])
+    pokemon1 = SelectField('Pokemon #1', choices=mychoices)
     poke1level = IntegerField("Pokemon #1 Level", default=1, validators=[])
-    pokemon2 = StringField('Pokemon #2 Name', validators=[])
+    #pokemon2 = StringField('Pokemon #2 Name', validators=[])
+    pokemon2 = SelectField('Pokemon #2', choices=mychoices)
     poke2level = IntegerField("Pokemon #2 Level", default=1, validators=[])
-    pokemon3 = StringField('Pokemon #3 Name', validators=[])
+    #pokemon3 = StringField('Pokemon #3 Name', validators=[])
+    pokemon3 = SelectField('Pokemon #3', choices=mychoices)
     poke3level = IntegerField("Pokemon #3 Level", default=1, validators=[])
-    pokemon4 = StringField('Pokemon #4 Name', validators=[])
+    #pokemon4 = StringField('Pokemon #4 Name', validators=[])
+    pokemon4 = SelectField('Pokemon #4', choices=mychoices)
     poke4level = IntegerField("Pokemon #4 Level", default=1, validators=[])
-    pokemon5 = StringField('Pokemon #5 Name', validators=[])
+    #pokemon5 = StringField('Pokemon #5 Name', validators=[])
+    pokemon5 = SelectField('Pokemon #5', choices=mychoices)
     poke5level = IntegerField("Pokemon #5 Level", default=1, validators=[])
-    pokemon6 = StringField('Pokemon #6 Name', validators=[])
+    #pokemon6 = StringField('Pokemon #6 Name', validators=[])
+    pokemon6 = SelectField('Pokemon #6', choices=mychoices)
     poke6level = IntegerField("Pokemon #6 Level", default=1, validators=[])
     submit = SubmitField('Create Team')
 
